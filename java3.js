98da4dbb742dc644cfdcbd13a851e4102d076ba5
shedwise <princeshedyabuchi@gmail.com>
2024-04-13 11:25:32 -0700

    glocery app

---
+++ java3.js
@@ -0,0 +1,150 @@
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+
+
+
+
+
+
+// ****** event listeners **********
+
+// submit form
+form.addEventListener("submit", addItem);
+
+
+//clear list
+clearBtn.addEventListener("click", clearItems);
+
+
+
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** functions *******
+
+//add item function
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+  //console.log(id)
+    if (value !== "" && !editFlag) {
+      const element = document.createElement("article");
+      let attr = document.createAttribute("data-id");
+      attr.value = id;
+      element.setAttributeNode(attr);
+    
+      element.classList.add("grocery-item");
+      element.innerHTML = `<p class="title">${value}</p>
+              <div class="btn-container">
+                <!-- edit btn -->
+                <button type="button" class="edit-btn">
+                  <i class="fas fa-edit"></i>
+                </button>
+                <!-- delete btn -->
+                <button type="button" class="delete-btn">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </div>
+            `;
+      
+            // add event listeners to both buttons inside the element;
+          const deleteBtn = element.querySelector(".delete-btn");
+           deleteBtn.addEventListener("click", deleteItem);
+           const editbtn =element.querySelector(".edit-btn")
+           editbtn.addEventListener("click", editItem)
+
+      // append child
+    list.appendChild(element);
+      // display alert
+      displayAlert("item added to the list", "success");
+      // show container
+      container.classList.add("show-container");
+      // set local storage
+
+      // set back to default
+      setBackToDefault();
+    } else if (value !== "" && editFlag) {
+      editElement.innerHTML = value;
+      displayAlert("value changed", "success");
+      setBackToDefault();
+    } else {
+        displayAlert("please enter value", "danger");
+      }
+}
+
+//clear items fun
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault();
+}
+
+
+
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    // remove alert
+    setTimeout(function () {
+      alert.textContent = "";
+      alert.classList.remove(`alert-${action}`);
+    }, 1000);
+  }
+
+
+
+  function deleteItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+  
+    list.removeChild(element);
+  
+    if (list.children.length === 0) {
+      container.classList.remove("show-container");
+    }
+    displayAlert("item removed", "danger");
+  
+    setBackToDefault();
+  }
+
+  
+  // edit item
+function editItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  // set form value
+  grocery.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  //
+  submitBtn.textContent = "edit";
+}
+
+
+
+
+//set backt to defaults
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "add";
+}
